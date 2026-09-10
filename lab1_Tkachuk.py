e3689e4f80ec846376cb2e58b215060c458493e9
TetianaTkachuk <tetanatkacuk194@gmail.com>
2026-09-10 18:40:22 +0300

    Add files via upload

---
+++ lab1_Tkachuk.py
@@ -0,0 +1,22 @@
+print("Hello,World!")
+a=10
+b=3.14
+c="Hello"
+d=True
+e=False
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+x=10
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x//y)
+print(x**y)
+print(x==y)
+
