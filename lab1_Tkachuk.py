28e60b7aeab90a9c9f2715c7e261cc4c6b9286d6
TetianaTkachuk <tetanatkacuk194@gmail.com>
2026-09-13 19:01:41 +0300

    Add new variables and print their types

--- lab1_Tkachuk.py
+++ lab1_Tkachuk.py
@@ -4,11 +4,19 @@ b=3.14
 c="Hello"
 d=True
 e=False
+f = [1, 2, 3]
+g = (1, 2, 3)
+h = {"name": "Тетяна", "age": 15}
+i = {1, 2, 3}
 print(a, type(a))
 print(b, type(b))
 print(c, type(c))
 print(d, type(d))
 print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+print(h, type(h))
+print(i, type(i))
 x=10
 y=3
 print(x+y)
@@ -19,4 +27,3 @@ print(x%y)
 print(x//y)
 print(x**y)
 print(x==y)
-
