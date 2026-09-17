8e9f225a187706c10b5018c25de699636b86cf97
TetianaTkachuk <tetanatkacuk194@gmail.com>
2026-09-17 17:26:08 +0300

    Update lab2_Tkachuk.py

--- lab2_Tkachuk.py
+++ lab2_Tkachuk.py
@@ -1,4 +1,7 @@
-korystuvachi = { "tanya": ["1234", [10, 11, 8, 12, 9, 4, 7]], "anna": ["qwerty", [9, 10, 6, 12, 3, 5]], "ivan": ["1111", [7, 8, 4, 2, 10, 11]], "maksym": ["2222", [12, 9, 10, 3, 4, 8]] }
+korystuvachi = {"tanya": ["1234", [10, 11, 8, 12, 9, 4, 7]], 
+                "anna": ["qwerty", [9, 10, 6, 12, 3, 5]], 
+                "ivan": ["1111", [7, 8, 4, 2, 10, 11]], 
+                "maksym": ["2222", [12, 9, 10, 3, 4, 8]]}
 login = input("Vvedit login: ")
 parol = input("Vvedit parol: ")
 if login in korystuvachi and parol == korystuvachi[login][0]:
